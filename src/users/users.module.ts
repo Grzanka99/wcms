7e13ed24a596dbe7defab16386f7e063d748accd
Cezary Grzanka <cezarygrzanka@gmail.com>
2020-06-05 14:51:44 +0200

    Quick fix, and update dependencie, node 14.4.0

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/entities/user.entity';
 
 @Module({
-  providers: [UsersService]
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  providers: [UsersService],
 })
 export class UsersModule {}
